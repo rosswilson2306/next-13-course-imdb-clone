769665492d30ea8ad04d718ad319ad45205b919f
Ross Wilson <ross.wilson@homeflow.co.uk>
2023-04-26 21:07:14 +0100

    added about page

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes'
 
 export default function Providers({ children }) {
   return (
-    <ThemeProvider enableSystem={true} attribue="class">
+    <ThemeProvider enableSystem={true} attribute="class">
       <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen select-none">
         {children}
       </div>
